c3b8fb336a36c5e0d351e5a093b39a0b38ca9d4d
Ashuraj Srivastav <ashuraj.srivastav01@gmail.com>
2025-11-23 21:19:20 +0530

    Currently learning API method

--- express_project/index.js
+++ express_project/index.js
@@ -1,11 +1,21 @@
 const express = require("express");
 let app = express();
+app.use(express.json());
 
 app.get("/", (req, res) => {
     res.send({
         status: true,
         message: "Home Route is working!"
-    })
+    });
+});
+
+// Login Post API
+app.post("/login", (req, res) => {
+    res.send({
+        status: true,
+        data: req.body,
+        message: "User Login API"
+    });
 });
 
 app.listen("1201");
